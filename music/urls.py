373c7a2b6711be15adb060aaf417a16c77d3d7fc
ArafinShams <aryancse247@gmail.com>
2017-07-05 18:22:48 +0600

    Completed video - 20

--- music/urls.py
+++ music/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     #/music/
     url(r'^$', views.index, name='index'),
-    #/music/71/
+    #/music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
 
